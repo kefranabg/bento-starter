dea342b971bac370ff163d81e42317aaad672ff4
Franck <franck.abgrall@zenika.com>
2019-02-03 12:15:21 +0100

    :sparkles: prerender webpack plugin added

--- src/router.js
+++ src/router.js
@@ -21,6 +21,7 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    },
+    { path: '*', redirect: '/' }
   ]
 })
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+const path = require('path')
+
 module.exports = {
+  configureWebpack: {
+    plugins: [
+      /* See https://github.com/chrisvfritz/prerender-spa-plugin for more details */
+      new PrerenderSPAPlugin({
+        // Required - The path to the webpack-outputted app to prerender.
+        staticDir: path.join(__dirname, 'dist'),
+        // Required - Routes to render.
+        routes: ['/', '/about']
+      })
+    ]
+  },
   pwa: {
     workboxOptions: {
       clientsClaim: true,
