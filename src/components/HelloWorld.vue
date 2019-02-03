e1b4d74838adc8a707f2906f2d48fcf9abd88636
Franck <franck.abgrall@zenika.com>
2019-02-03 17:09:02 +0100

    :wrench: update eslint config

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,52 +1,56 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }} {{newContentAvailable ? 'oui' : 'non'}}4</h1>
+    <h1>{{ msg }}</h1>
 
     <p>
       For a guide and recipes on how to configure / customize this project,
-      <br>check out the
-      <a
-        href="https://cli.vuejs.org"
-        target="_blank"
-        rel="noopener"
-      >vue-cli documentation</a>.
+      <br />check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
+        >vue-cli documentation</a
+      >.
     </p>
     <h3>Installed CLI Plugins</h3>
+
     <ul>
       <li>
         <a
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
           target="_blank"
           rel="noopener"
-        >babel</a>
+          >babel</a
+        >
       </li>
       <li>
         <a
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa"
           target="_blank"
           rel="noopener"
-        >pwa</a>
+          >pwa</a
+        >
       </li>
       <li>
         <a
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
           target="_blank"
           rel="noopener"
-        >eslint</a>
+          >eslint</a
+        >
       </li>
       <li>
         <a
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest"
           target="_blank"
           rel="noopener"
-        >unit-jest</a>
+          >unit-jest</a
+        >
       </li>
       <li>
         <a
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-e2e-cypress"
           target="_blank"
           rel="noopener"
-        >e2e-cypress</a>
+          >e2e-cypress</a
+        >
       </li>
     </ul>
     <h3>Essential Links</h3>
@@ -55,13 +59,19 @@
         <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
       </li>
       <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a>
+        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
+          >Forum</a
+        >
       </li>
       <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a>
+        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
+          >Community Chat</a
+        >
       </li>
       <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a>
+        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
+          >Twitter</a
+        >
       </li>
       <li>
         <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
@@ -70,7 +80,9 @@
     <h3>Ecosystem</h3>
     <ul>
       <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a>
+        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
+          >vue-router</a
+        >
       </li>
       <li>
         <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
@@ -80,13 +92,21 @@
           href="https://github.com/vuejs/vue-devtools#vue-devtools"
           target="_blank"
           rel="noopener"
-        >vue-devtools</a>
+          >vue-devtools</a
+        >
       </li>
       <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a>
+        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
+          >vue-loader</a
+        >
       </li>
       <li>
-        <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+          rel="noopener"
+          >awesome-vue</a
+        >
       </li>
     </ul>
   </div>
--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import store from './store'
 import './handle-network-status'
 
 OfflinePluginRuntime.install()
+
 Vue.config.productionTip = false
 
 new Vue({
