da54ea6060cad0351e38fe6fe6a74dc58488ad8c
Franck <franck.abgrall@zenika.com>
2019-03-03 10:06:02 +0100

    :sparkles: try to blacklist route for sw redirection

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,12 +12,9 @@ self.__precacheManifest = [].concat(self.__precacheManifest || [])
 workbox.precaching.suppressWarnings()
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
 
-workbox.routing.registerNavigationRoute(
-  workbox.precaching.getCacheKeyForURL('/index.html'),
-  {
-    blacklist: [new RegExp('authType=signInViaRedirect')]
-  }
-)
+workbox.routing.registerNavigationRoute('/index.html', {
+  blacklist: [new RegExp('/__/auth/handler')]
+})
 
 workbox.routing.registerRoute(
   /^https:\/\/fonts/,
