bc1805980d68de3462bf9c9cf4e099ba1e412a4b
Franck Abgrall <abgrallkefran@gmail.com>
2019-03-30 11:21:37 +0100

    :white_check_mark: Review unit tests (#78)

--- tests/unit/misc/helpers.spec.js
+++ tests/unit/misc/helpers.spec.js
@@ -5,21 +5,11 @@ jest.mock('@/firebase/users-db', () =>
   jest.fn().mockImplementation(() => ({ create: mockCreate }))
 )
 
-const user = {
-  displayName: 'Robert Bob',
-  photoUrl: 'https://my-awesome-photo.com',
-  email: 'robert.bob@mail.com'
-}
-const newUser = {
-  displayName: 'New user',
-  photoUrl: 'https://new-user-photo.com',
-  email: 'new.user@mail.com'
-}
-
 describe('helpers', () => {
   describe('createNewUserFromFirebaseAuthUser', () => {
     it('should set user with the created user', async () => {
-      const firebaseUser = { providerData: [user] }
+      const firebaseUser = { providerData: [{ id: 1 }] }
+      const newUser = { id: 2 }
       mockCreate.mockResolvedValue(Promise.resolve(newUser))
       const createdUser = await createNewUserFromFirebaseAuthUser(firebaseUser)
 
--- tests/unit/store/products/products.getters.spec.js
+++ tests/unit/store/products/products.getters.spec.js
@@ -1,14 +1,15 @@
 import getters from '@/store/products/products.getters'
 
+const state = { productDeletionPending: [1, 2, 3] }
+
 describe('products module getters', () => {
   describe('isProductDeletionPending', () => {
     it('should return true if the given product id is marked as pending', () => {
-      const state = { productDeletionPending: [1, 2, 3] }
       const result = getters.isProductDeletionPending(state)(1)
       expect(result).toBe(true)
     })
+
     it('should return false if the given product id is not marked as pending', () => {
-      const state = { productDeletionPending: [1, 2, 3] }
       const result = getters.isProductDeletionPending(state)(4)
       expect(result).toBe(false)
     })
