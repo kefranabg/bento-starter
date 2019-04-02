0e58c5aa6a71d1ea98fbff6a2768f28156f882dd
Franck Abgrall <abgrallkefran@gmail.com>
2019-04-02 18:39:01 +0200

    :green_apple: Fix IOS bug (#85)

--- src/misc/handle-apple-install-prompt.js
+++ src/misc/handle-apple-install-prompt.js
@@ -1,19 +1,23 @@
 import store from '@/store'
+import { isNil } from 'lodash'
 
-const showPromptForIos =
+const isIosOnBrowser =
   ['iPhone', 'iPad', 'iPod'].includes(navigator.platform) &&
   !window.navigator.standalone
 
-if (showPromptForIos) {
+if (isIosOnBrowser) {
   const now = Date.now()
+  let limitDate = null
   const addToHomeIosPromptLastDate = localStorage.getItem(
     'addToHomeIosPromptLastDate'
   )
-  const limitDate = addToHomeIosPromptLastDate
-    ? new Date(parseInt(addToHomeIosPromptLastDate))
-    : new Date()
-  limitDate.setMonth(addToHomeIosPromptLastDate.getMonth() + 1)
-  if (now >= limitDate.getTime()) {
+
+  if (!isNil(addToHomeIosPromptLastDate)) {
+    limitDate = new Date(parseInt(addToHomeIosPromptLastDate))
+    limitDate.setMonth(limitDate.getMonth() + 1)
+  }
+
+  if (isNil(limitDate) || now >= limitDate.getTime()) {
     store.commit('app/setShowAddToHomeScreenModalForApple', true)
   }
 }
