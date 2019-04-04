b71ea9f182e7ae0fc0018c7c5abc30ad66be55c5
Franck <franck.abgrall@zenika.com>
2019-04-04 13:42:03 +0200

    :art: Add pwa compat file to project

--- src/misc/pwacompat.js
+++ src/misc/pwacompat.js
@@ -1,20 +1,3 @@
-/*
- * Copyright 2018 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
-
-'use strict'
 ;(function() {
   // basic feature detection: from IE10+
   // also fallout on 'navigator.standalone', we _are_ an iOS PWA
@@ -74,7 +57,9 @@
       try {
         new URL('', opt)
         return part => new URL(part || '', opt).toString()
-      } catch (e) {}
+      } catch (e) {
+        console.err(e)
+      }
     }
     return part => part || ''
   }
@@ -402,6 +387,7 @@
    */
   function getEdgeTitleBar() {
     try {
+      // eslint-disable-next-line no-undef
       return Windows.UI.ViewManagement.ApplicationView.getForCurrentView()
         .titleBar
     } catch (e) {
