844f17f761109b08ddc9c3461af00b489a25fb7c
Franck <franck.abgrall@zenika.com>
2019-04-04 19:49:25 +0200

    :lipstick: Add documentation link to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="page-wrapper">
     <h1 class="home-page-title">{{ appTitle }}</h1>
-    <img src="@/assets/img/bento-starter.svg" />
+    <img class="logo" src="@/assets/img/bento-starter.svg" />
+
+    <a
+      class="documentation-link"
+      target="_blank"
+      href="https://bento-starter.netlify.com/"
+      >Documentation →</a
+    >
   </div>
 </template>
 
@@ -28,9 +35,31 @@ export default {
 .page-wrapper {
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .logo {
+    margin-bottom: 3rem;
+  }
 
   .home-page-title {
+    margin-top: 3rem;
     text-align: center;
+  }
+
+  .documentation-link {
+    display: inline-block;
+    font-size: 1.2rem;
+    color: #fff;
+    background-color: #3eaf7c;
+    padding: 0.8rem 1.6rem;
+    border-radius: 4px;
+    transition: background-color 0.1s ease;
+    box-sizing: border-box;
+    border-bottom: 1px solid #389d70;
+    text-decoration: none;
+    width: fit-content;
+    font-weight: 500;
   }
 }
 </style>
