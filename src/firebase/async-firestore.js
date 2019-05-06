178e617457c8814d8b724cba7661391c4690f672
Franck Abgrall <abgrallkefran@gmail.com>
2019-05-06 19:37:22 +0200

    :bug: Fix async firestore import (#154)

--- src/firebase/async-firestore.js
+++ src/firebase/async-firestore.js
@@ -1,17 +1,21 @@
 import firebase from 'firebase/app'
+import isNil from 'lodash/isNil'
 
-let loaded = false
+let asyncFirestore = null
 
 // Lazy load firestore with async import is important for performance
 
-export default async () => {
-  if (!loaded) {
-    await import(/* webpackChunkName: "chunk-firestore" */ 'firebase/firestore')
-    firebase.firestore().settings({})
-    firebase
-      .firestore()
-      .enablePersistence({ experimentalTabSynchronization: true })
-    loaded = true
+export default () => {
+  if (isNil(asyncFirestore)) {
+    asyncFirestore = import(/* webpackChunkName: "chunk-firestore" */ 'firebase/firestore').then(
+      () => {
+        firebase.firestore().settings({})
+        firebase
+          .firestore()
+          .enablePersistence({ experimentalTabSynchronization: true })
+        return firebase.firestore()
+      }
+    )
   }
-  return firebase.firestore()
+  return asyncFirestore
 }
