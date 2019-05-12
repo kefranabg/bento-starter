7476d95a5531b2e2339e8638f4d0a2e9d9b54a66
Franck <franck.abgrall@zenika.com>
2019-05-12 14:51:21 +0200

    :sparkles: Add assetlinks file for android apk check

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,7 +12,11 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
 // Redirect to index.html if sw cannot find matching route
 workbox.routing.registerNavigationRoute('/index.html', {
   /* Do not redirect routes used by firebase auth  */
-  blacklist: [new RegExp('/__/auth/handler'), new RegExp('/__/auth/iframe')]
+  blacklist: [
+    new RegExp('/__/auth/handler'),
+    new RegExp('/__/auth/iframe'),
+    new RegExp('/.well-known')
+  ]
 })
 
 workbox.routing.registerRoute(
