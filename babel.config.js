fade828f92b5bc238f498521cd0f630e04fe06cb
Franck <franck.abgrall@zenika.com>
2019-05-20 18:37:51 +0200

    :ok_hand: Remove babel env preset

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,4 @@
 module.exports = {
   plugins: ['lodash'],
-  presets: [
-    ['@vue/app', { useBuiltIns: 'entry' }],
-    ['@babel/env', { targets: { node: 6 } }]
-  ]
+  presets: [['@vue/app', { useBuiltIns: 'entry' }]]
 }
