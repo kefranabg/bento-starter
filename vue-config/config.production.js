d1a15bb9934dd03ab647661f80b12c9b3818932c
Chidindu Ogbonna <promise.bones@gmail.com>
2019-07-12 11:49:59 +0100

    PrerenderSPAPlugin in config.production.js
    PrerenderSPAPlugin should be used in production by default.

--- vue-config/config.production.js
+++ vue-config/config.production.js
@@ -1,5 +1,9 @@
+const path = require('path')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer') // eslint-disable-line
   .BundleAnalyzerPlugin
+const PrerenderSPAPlugin = require('prerender-spa-plugin') // eslint-disable-line
+
+const prerenderedRoutesList = ['/login', '/home', '/']
 
 module.exports = {
   configureWebpack: {
@@ -8,6 +12,14 @@ module.exports = {
       new BundleAnalyzerPlugin({
         analyzerMode: 'disabled',
         generateStatsFile: true
+      }),
+      
+      /* See https://github.com/chrisvfritz/prerender-spa-plugin for more details */
+      new PrerenderSPAPlugin({
+        // Required - The path to the webpack-outputted app to prerender.
+        staticDir: path.join(__rootDirname),
+        // Required - Routes to prerender.
+        routes: prerenderedRoutesList
       })
     ]
   }
