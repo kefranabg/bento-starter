014a5b81d6e1ba9e23ed4b90bd4f70759400583f
Chidindu Ogbonna <promise.bones@gmail.com>
2019-07-12 11:50:02 +0100

    Remove PrerenderSPAPlugin from config.default.js
    PrerenderSPAPlugin should not be used in config.default.js as this affects hot-reloading in dev environment causing changes to show slowly in development.

--- vue-config/config.default.js
+++ vue-config/config.default.js
@@ -1,7 +1,4 @@
-const PrerenderSPAPlugin = require('prerender-spa-plugin') // eslint-disable-line
 const path = require('path')
-
-const prerenderedRoutesList = ['/login', '/home', '/']
 
 module.exports = {
   /* See https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa for more details */
@@ -12,16 +9,5 @@ module.exports = {
     workboxOptions: {
       swSrc: path.join('public', 'service-worker.js')
     }
-  },
-  configureWebpack: {
-    plugins: [
-      /* See https://github.com/chrisvfritz/prerender-spa-plugin for more details */
-      new PrerenderSPAPlugin({
-        // Required - The path to the webpack-outputted app to prerender.
-        staticDir: path.join(__rootDirname),
-        // Required - Routes to prerender.
-        routes: prerenderedRoutesList
-      })
-    ]
   }
 }
